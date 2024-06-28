0e03c591ce3ab3a5b5e009c530ba913776083793
Praise Ekanem <basticks@meta.com>
2024-06-28 14:00:35 -0700

    Added login functionality on Backend

--- backend/user.js
+++ backend/user.js
@@ -5,6 +5,8 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const router = express.Router();
 
+//const SECRET_KEY = process.env.SECRET_KEY
+
 //Signup
 router.post('/signup', async (req, res) => {
     const { username, email, password, role } = req.body;
@@ -31,6 +33,34 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+//Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Please fill out all fields' });
+    }
+
+    try{
+        const user = await prisma.user.findUnique({ where: { username } });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+
+        //TODO: Add Session commsnd here
+
+        res.status(200).json({message:'Login Successsful', user})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Login failed' });
+    }
+})
 
 module.exports = router;
