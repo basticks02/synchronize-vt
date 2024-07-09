3a1e3366d5c7f3b4d75e85aee47823d3de000844
Praise Ekanem <basticks@meta.com>
2024-07-09 15:03:21 -0700

    Layout for the Discover page

--- frontend/src/Discover/Discover.jsx
+++ frontend/src/Discover/Discover.jsx
@@ -16,6 +16,29 @@ export default function Locate() {
                     <h1>DISCOVER</h1>
                 </div>
             </section>
+
+            <section className='covid-resources'>
+                <div className='covid-container'>
+                    <div className='covid-usa'>
+                        <h3>Covid USA</h3>
+                    </div>
+                    <div className='covid-worldwide'>
+                        <h3>Covid WorldWide</h3>
+                    </div>
+                </div>
+
+                <div className='covid-vaccines'>
+                    <h3>Covid Vaccines Worldwide</h3>
+                </div>
+
+                <div className='covid-history'>
+                    <h3>Covid History</h3>
+                </div>
+            </section>
+
+            <footer>
+                <p>Developed by Synchronize</p>
+            </footer>
         </main>
     </>
   )
