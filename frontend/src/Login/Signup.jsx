435a551dd089ae4b9b17f7f8a3aa5000e6f7d06d
Praise Ekanem <basticks@meta.com>
2024-07-26 12:18:58 -0700

    Refactored the Login/Signup components using a Authcontainer component to reduce code repetition.

--- frontend/src/Login/Signup.jsx
+++ frontend/src/Login/Signup.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useContext} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-import {UserContext} from '../UserContext'
 import api from '../api'
+import AuthContainer from './Authcontainer'
 
 
 export default function Signup() {
@@ -39,68 +39,53 @@ export default function Signup() {
     }
 
   return (
-    <div className='logincontainer'>
-        <div className='welcomecontainer'>
-          <h1>Welcome!</h1>
-          <p>Commited to service excellence</p>
-          <div className='socialmediaicons'>
-              <i className="fa-brands fa-facebook"></i>
-              <i className="fa-brands fa-twitter"></i>
-              <i className="fa-brands fa-instagram"></i>
-              <i className="fa-solid fa-envelope"></i>
-          </div>
+    <AuthContainer
+      title="Sign Up"
+      footerText="Already have an account?"
+      footerLink="/login"
+      footerLinkText="Sign In"
+    >
+      <form onSubmit={handleSignup}>
+        <div className='entry'>
+          <label htmlFor="Username">Username</label>
+          <input
+            type="text"
+            id='username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder='First name (recommended)'
+            required
+          />
         </div>
-
-        <div className='authenticationcontainer'>
-          <h2>Sign Up</h2>
-          <form onSubmit={handleSignup}>
-
-            <div className='entry'>
-              <label htmlFor="Username">Username</label>
-              <input
-                type="text"
-                id='username'
-                value={username}
-                onChange={(e)=> setUsername(e.target.value)}
-                placeholder='First name (recommended)'
-                required  />
-            </div>
-
-            <div className='entry'>
-              <label htmlFor="Email Address">Email Address</label>
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e)=> setEmail(e.target.value)}
-                placeholder='synchronize@gmail.com'
-                required />
-            </div>
-
-            <div className='entry'>
-              <label htmlFor="Password">Create Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e)=> setPassword(e.target.value)}
-                />
-            </div>
-
-            <div className='userRole' >
-                <select id="role" value={role} onChange={(e)=> setRole(e.target.value)}>
-                    <option value="">Role</option>
-                    <option value="patient">Patient</option>
-                    <option value="physician">Physician</option>
-                </select>
-            </div>
-
-            <button className="signupbutton" type='submit'>Sign Up</button>
-          </form>
-            <p>Already have an account? <Link to="/login">Sign In</Link> </p>
-            <Link to="/" >Return to Home</Link>
-
+        <div className='entry'>
+          <label htmlFor="Email Address">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='synchronize@gmail.com'
+            required
+          />
         </div>
-    </div>
+        <div className='entry'>
+          <label htmlFor="Password">Create Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='userRole'>
+          <select id="role" value={role} onChange={(e) => setRole(e.target.value)}>
+            <option value="">Role</option>
+            <option value="patient">Patient</option>
+            <option value="physician">Physician</option>
+          </select>
+        </div>
+        <button className="signupbutton" type='submit'>Sign Up</button>
+      </form>
+    </AuthContainer>
   )
 }
